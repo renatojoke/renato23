8559782fcb60ee36439904ae6a32d2779437dcf5
renato <120775869+renatojoke@users.noreply.github.com>
2023-08-01 16:02:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$str = "abc";
+$intenger = 34;
+$bool = false;
+
+echo $bool;
+
+
+if (false){
+  echo"true";
+}
+else {
+  echo"False";
+}
+
+$num1 = 5;
+if($num1 = "5");
+{
+  echo"true";
+
+}
+else {
+  echo"False";
+}
+
+
+$num2 = 5;
+if($num2 <= "5"&& $num2 >5);
+{
+  echo"true";
+
+}
+else {
+  echo"False";
+}
+?>
